581130e94b3e83f9f0adc0057838b0bdf6d22804
Douglas Fuelber <dev@douglasfuelber.com>
2024-03-12 23:49:21 -0300

    Adjusts in pipeline

--- s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "burgerroyale_auth_s3_bucket" {
-    bucket = var.bucketName
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.burgerroyale_auth_s3_bucket.id
-  acl    = "public-read"
-}
