1a9fe2ad721c6bb7ab519f439787a526829ca31e
Douglas Fuelber <dev@douglasfuelber.com>
2024-03-12 23:02:21 -0300

    Changes in provider

--- provider.tf
+++ provider.tf
@@ -1,17 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      Version = "~>3.27"
-    }
-  }
-
-  required_version = ">=0.14.9"
-
   backend "s3" {
-    bucket = var.bucketName
+    bucket = "burgerroyale-auth-s3-bucket"
     key = "state/terraform.tfstate"
-    region = var.regionDefault
+    region = "us-east-1"
   }
 }
 
